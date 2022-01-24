7f42709085b78652f3533496588d34cd079260f6
Behfar Lotfizadeh <behfar@otafest.com>
2022-01-23 23:02:05 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace esthar_practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_stepId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_stepFraction = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_totalEncounters = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_dangerValue = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_offset = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_prevEncId = new System.Windows.Forms.TextBox();
+            this.btn_go = new System.Windows.Forms.Button();
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_stepId
+            // 
+            this.txt_stepId.Location = new System.Drawing.Point(106, 6);
+            this.txt_stepId.Name = "txt_stepId";
+            this.txt_stepId.Size = new System.Drawing.Size(49, 20);
+            this.txt_stepId.TabIndex = 3;
+            this.txt_stepId.Text = "29";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Step ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Step Fraction";
+            // 
+            // txt_stepFraction
+            // 
+            this.txt_stepFraction.Location = new System.Drawing.Point(106, 32);
+            this.txt_stepFraction.Name = "txt_stepFraction";
+            this.txt_stepFraction.Size = new System.Drawing.Size(49, 20);
+            this.txt_stepFraction.TabIndex = 5;
+            this.txt_stepFraction.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Total Encounters";
+            // 
+            // txt_totalEncounters
+            // 
+            this.txt_totalEncounters.Location = new System.Drawing.Point(106, 58);
+            this.txt_totalEncounters.Name = "txt_totalEncounters";
+            this.txt_totalEncounters.Size = new System.Drawing.Size(49, 20);
+            this.txt_totalEncounters.TabIndex = 7;
+            this.txt_totalEncounters.Text = "22";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(161, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Danger Value";
+            // 
+            // txt_dangerValue
+            // 
+            this.txt_dangerValue.Location = new System.Drawing.Point(239, 6);
+            this.txt_dangerValue.Name = "txt_dangerValue";
+            this.txt_dangerValue.Size = new System.Drawing.Size(49, 20);
+            this.txt_dangerValue.TabIndex = 9;
+            this.txt_dangerValue.Text = "80";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(161, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Offset";
+            // 
+            // txt_offset
+            // 
+            this.txt_offset.Location = new System.Drawing.Point(239, 32);
+            this.txt_offset.Name = "txt_offset";
+            this.txt_offset.Size = new System.Drawing.Size(49, 20);
+            this.txt_offset.TabIndex = 11;
+            this.txt_offset.Text = "13";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(161, 61);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Prev Enc ID";
+            // 
+            // txt_prevEncId
+            // 
+            this.txt_prevEncId.Location = new System.Drawing.Point(239, 58);
+            this.txt_prevEncId.Name = "txt_prevEncId";
+            this.txt_prevEncId.Size = new System.Drawing.Size(49, 20);
+            this.txt_prevEncId.TabIndex = 13;
+            this.txt_prevEncId.Text = "0";
+            // 
+            // btn_go
+            // 
+            this.btn_go.Location = new System.Drawing.Point(15, 84);
+            this.btn_go.Name = "btn_go";
+            this.btn_go.Size = new System.Drawing.Size(273, 23);
+            this.btn_go.TabIndex = 15;
+            this.btn_go.Text = "Go";
+            this.btn_go.UseVisualStyleBackColor = true;
+            this.btn_go.Click += new System.EventHandler(this.Btn_Go_Click);
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.Location = new System.Drawing.Point(15, 110);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(273, 17);
+            this.lbl_status.TabIndex = 16;
+            this.lbl_status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 136);
+            this.Controls.Add(this.lbl_status);
+            this.Controls.Add(this.btn_go);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_prevEncId);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_offset);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_dangerValue);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_totalEncounters);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_stepFraction);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_stepId);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(320, 175);
+            this.MinimumSize = new System.Drawing.Size(320, 175);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FF8 Esthar Practice";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txt_stepId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_stepFraction;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_totalEncounters;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_dangerValue;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_offset;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_prevEncId;
+        private System.Windows.Forms.Button btn_go;
+        private System.Windows.Forms.Label lbl_status;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+using System.Media;
+using ProcessMemoryReaderLib;
+using System.Runtime.InteropServices;
+
+namespace esthar_practice
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern
+       bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, int vk);
+        [DllImport("user32")]
+        public static extern
+        bool GetMessage(ref Message lpMsg, IntPtr handle, uint mMsgFilterInMain, uint mMsgFilterMax);
+
+        public const int MOD_ALT = 0x0001;
+        public const int MOD_CONTROL = 0x0002;
+        public const int MOD_SHIFT = 0x004;
+        public const int MOD_NOREPEAT = 0x400;
+        public const int WM_HOTKEY = 0x312;
+        public const int DSIX = 0x36;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Set an unique id to your Hotkey, it will be used to
+            // identify which hotkey was pressed in your code to execute something
+            int UniqueHotkeyId = 1;
+            // Set the Hotkey triggerer the F9 key 
+            // Expected an integer value for F9: 0x78, but you can convert the Keys.KEY to its int value
+            // See: https://msdn.microsoft.com/en-us/library/windows/desktop/dd375731(v=vs.85).aspx
+            int HotKeyCode = (int)Keys.F6;
+            // Register the "F9" hotkey
+            Boolean F9Registered = RegisterHotKey(
+                this.Handle, UniqueHotkeyId, 0x0000, HotKeyCode
+            );
+
+            // 4. Verify if the hotkey was succesfully registered, if not, show message in the console
+            if (F9Registered)
+            {
+                Console.WriteLine("Global Hotkey F9 was succesfully registered");
+            }
+            else
+            {
+                Console.WriteLine("Global Hotkey F9 couldn't be registered !");
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            // 5. Catch when a HotKey is pressed !
+            if (m.Msg == 0x0312)
+            {
+                int id = m.WParam.ToInt32();
+                // MessageBox.Show(string.Format("Hotkey #{0} pressed", id));
+
+                if (id == 1)
+                {
+                    Begin();
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Btn_Go_Click(object sender, EventArgs e)
+        {
+            Begin();
+        }
+
+        private string GetVersion(Process ff8Game)
+        {
+            // Get the language from the process name (i.e. remove "FF8_" from the name)
+            string GameVersion = ff8Game.ProcessName.Substring(4);
+
+            // Update Form Title
+            SetFormText("FF8 Esthar Practice - " + GameVersion);
+
+            // DO STUFF
+            return GameVersion;
+        }
+        private Process FindGame()
+        {
+            List<Process> processes = new List<Process>();
+                processes = Process.GetProcesses()
+                .Where(x => (x.ProcessName.StartsWith("FF8_", StringComparison.OrdinalIgnoreCase))
+                            && !(x.ProcessName.Equals("FF8_Launcher", StringComparison.OrdinalIgnoreCase)))
+                .ToList();
+
+            if (processes.Count == 0)
+                throw new NullReferenceException("Game not found.");
+
+            return processes[0];
+        }
+
+
+        // Thread-Safe Call to Windows Forms Control
+        delegate void SetFormTextCallback(string text);
+        public void SetFormText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.InvokeRequired)
+            {
+                SetFormTextCallback d = new SetFormTextCallback(SetFormText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.Text = text;
+            }
+        }
+        // Thread-Safe Call to Windows Forms Control
+        delegate void SetStatusTextCallback(string text);
+        public void SetStatusText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.lbl_status.InvokeRequired)
+            {
+                SetStatusTextCallback d = new SetStatusTextCallback(SetStatusText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.lbl_status.Text = text;
+            }
+        }
+        public async void Begin()
+        {
+            Process ff8Game = new Process();
+            IntPtr GameBaseAddress = new IntPtr();
+            string version = "";
+
+            try {
+                // Find the FF8 process.
+                ff8Game = await Task.Run(FindGame);
+                GameBaseAddress = ff8Game.MainModule.BaseAddress;
+                version = GetVersion(ff8Game).ToUpper();
+
+                SetStatusText("Game found: " + version);
+            }
+            catch (NullReferenceException e)
+            {
+                SetStatusText(e.Message);
+                return;
+            }
+
+            // FR address = EN address - hex(328)
+            int hexOffset = (version == "EN") ? 0x0 : 0x328;
+
+            // Addresses (manipulate to correct address if FR)
+            IntPtr addressStepId = IntPtr.Add(GameBaseAddress, 0x18D2FB8 - hexOffset);
+            IntPtr addressStepFrac = IntPtr.Add(GameBaseAddress, 0x18DC740 - hexOffset);
+            IntPtr addressTotalEncs = IntPtr.Add(GameBaseAddress, 0x18DBFEC - hexOffset);
+            IntPtr addressDangerValue = IntPtr.Add(GameBaseAddress, 0x18DC74A - hexOffset);
+            IntPtr addressOffset = IntPtr.Add(GameBaseAddress, 0x18DC748 - hexOffset);
+            IntPtr addressLastEncId = IntPtr.Add(GameBaseAddress, 0x1996DA8 - hexOffset);
+
+            ProcessMemoryReader reader = new ProcessMemoryReader
+            {
+                ReadProcess = ff8Game
+            };
+            reader.OpenProcess();
+
+            byte[] stepId, StepFrac, TotalEncs, DangerValue, Offset, LastEncId;
+            try
+            {
+                stepId = BitConverter.GetBytes(Convert.ToInt32(txt_stepId.Text));
+                StepFrac = BitConverter.GetBytes(Convert.ToInt32(txt_stepFraction.Text));
+                TotalEncs = BitConverter.GetBytes(Convert.ToInt32(txt_totalEncounters.Text));
+                DangerValue = BitConverter.GetBytes(Convert.ToInt32(txt_dangerValue.Text));
+                Offset = BitConverter.GetBytes(Convert.ToInt32(txt_offset.Text));
+                LastEncId = BitConverter.GetBytes(Convert.ToInt32(txt_prevEncId.Text));
+
+                // Write to game memory.
+                reader.WriteProcessMemory(addressStepId, stepId, out int _);                
+                reader.WriteProcessMemory(addressStepFrac, StepFrac, out int _);
+                reader.WriteProcessMemory(addressTotalEncs, TotalEncs, out int _);
+                reader.WriteProcessMemory(addressOffset, Offset, out int _);
+                reader.WriteProcessMemory(addressLastEncId, LastEncId, out int _);
+
+                // Danger value won't update if you assign it simultaenously.
+                Thread.Sleep(100);
+                reader.WriteProcessMemory(addressDangerValue, DangerValue, out int _);
+
+                SystemSounds.Exclamation.Play();
+            }
+            catch (Exception e)
+            {
+                SetStatusText("Error parsing values.");
+                return;
+            }
+        }
+    }
+
+}
